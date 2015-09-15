2e6da04373d00494aab79345d4d9618cd9b44162
Stan Dyck <stan.dyck@gmail.com>
2015-09-15 16:56:56 -0700

    Added find-common-prefix and find-common-suffix functions.

--- src/cleancode/core.clj
+++ src/cleancode/core.clj
@@ -13,6 +13,19 @@
   (str/trim (str msg " expected:<[" expected "]> but was:<[" actual "]>")))
 
 
+(defn find-common-prefix
+  "Returns the first characters that s1 s2 share in common."
+  [s1 s2]
+  (apply str
+         (map first
+              (take-while #(= (first %) (last %))
+                          (partition 2 (interleave s1 s2))))))
+
+(defn find-common-suffix
+  [s1 s2]
+  (str/reverse (find-common-prefix (str/reverse s1) (str/reverse s2))))
+
+
 (defn compact
   ([len expected actual msg] (format msg expected actual))
   ([len expected actual]     (compact len expected actual nil)))
